4cf56e81ff310054318345bd0b176c086a2fc21b
Christian Pinto <pintochristian2@gmail.com>
2019-03-31 09:36:28 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,7 +1,7 @@
 def introduction(name)
-  puts "Hi, my name is #{name}\n"
+  puts "Hi, my name is #{name}.\n"
 end
 
 def introduction_to_language(name, language)
-  puts "Hi, my name is #{name} and i am learning to program in #{language}\n"
+  puts "Hi, my name is #{name} and i am learning to program in #{language}.\n"
 end
